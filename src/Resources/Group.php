895b5f469db310635a35198167ebd2ca20fdf9ae
Rafael Milewski <rafael.milewski@gmail.com>
2022-02-04 21:57:20 +0800

    Update Group.php

--- src/Resources/Group.php
+++ src/Resources/Group.php
@@ -10,7 +10,7 @@ class Group extends TopLevelResource
 
     public function authorizedToSee(Request $request)
     {
-        $authorized = collect($this->resources())->some(function (string $resource) use ($request) {
+        $authorized = collect($this->resources())->some(function (AbstractResource|string $resource) use ($request) {
             return $this->parseResource($resource)->authorizedToSee($request);
         });
 
