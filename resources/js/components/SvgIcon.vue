cc0e97786e19b9c411b06abbce5fa554d63bb89c
Rafael Milewski <rafael.milewski@gmail.com>
2023-10-07 13:41:49 +0800

    update documentation

---
+++ resources/js/components/SvgIcon.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <template v-if="type.trimStart().startsWith('<svg')">
+        <div v-html="type"/>
+    </template>
+
+    <Icon v-else :type="type" :width="width"/>
+
+</template>
+
+<script>
+
+    export default {
+        props: [ 'type', 'width' ],
+    }
+
+</script>
